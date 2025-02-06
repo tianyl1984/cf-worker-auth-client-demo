d93f68e7efa98209b0128006515e12c54d956a5e
tyl <tianyl1984@qq.com>
2025-02-06 17:31:31 +0800

    add log

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ async function _handle(req, env, ctx) {
 	if (authResult.resp != null) {
 		return authResult.resp;
 	}
+	console.log('login success');
 	const username = authResult.username;
 	return new Response(`Hello:${username}`);
 }
